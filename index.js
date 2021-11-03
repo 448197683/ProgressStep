299279610fc2d153bce22b6979fc475182a116fb
448197683 <448197683@qq.com>
2021-11-03 14:20:10 +0800

    finish

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const circle = document.querySelectorAll('.circle');
+const circles = document.querySelectorAll('.circle');
 const prevBtn = document.querySelector('.prevBtn');
 const nextBtn = document.querySelector('.nextBtn');
 const line = document.querySelector('.line-over');
-const totalNum = circle.length;
+const totalNum = circles.length;
 
 let currentNum = 1;
 
@@ -13,16 +13,30 @@ const handleNext = (e) => {
   }
   line.style.width = `${((currentNum - 1) / (totalNum - 1)) * 100}%`;
   prevBtn.disabled = false;
+  circles.forEach((circle, index) => {
+    if (currentNum > index) {
+      circle.classList.add('active');
+    } else {
+      circle.classList.remove('active');
+    }
+  });
 };
-
 const handlePrev = (e) => {
   currentNum--;
+  console.log(currentNum);
   if (currentNum <= 1) {
     prevBtn.disabled = true;
   }
   line.style.width = `${((currentNum - 1) / (totalNum - 1)) * 100}%`;
   nextBtn.disabled = false;
   console.log(currentNum);
+  circles.forEach((circle, index) => {
+    if (currentNum > index) {
+      circle.classList.add('active');
+    } else {
+      circle.classList.remove('active');
+    }
+  });
 };
 
 nextBtn.addEventListener('click', handleNext);
